8d2a65c4c6625f4feec34a1441f8a93401eb1750
srinukanna <kannasrinivasulu8@gmail.com>
2026-03-12 18:30:34 +0530

    mycpp

--- Basic_topics/i/I/input/output.cpp
+++ Basic_topics/i/I/input/output.cpp
@@ -3,7 +3,24 @@ using namespace std;
 
 int main()
 {
-    string name;
-    cin>>name;
-    cout<<"Hello";
+    //int,char,float,long,double
+
+    //datatypes,conditional ,arrays,oops,this keyword ,loops
+    int a;
+    char c;
+    float b;
+    long long l;
+    double d;
+
+
+    cout<<"Enter the integer";
+    cin>>a;
+
+    cout<<"Enter the character value";
+    cin>>c;
+
+    cout<<"Enter the double value";
+    cin>>d;
+
+    
 }
---
+++ practice/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+for(int i=0;i<2;i++){
+    cout<<"i am in the i loop"<<endl;
+    for(int j=0;j<4;j++){
+        cout<<"i am in the j loop"<<endl;
+    }
+}
+
+}
+
+// variables,conditionals,loops behaviour,switch case
